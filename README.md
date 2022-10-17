2728a5acdb645f93ef223a0bc15dd3545fd82c51
Amir Boussejra <amir@epigno.jp>
2022-10-17 10:25:25 +0200

    [rust 1.64] Code not building

---
+++ README.md
@@ -0,0 +1,1 @@
+# rust_compiler_1.64_update_problem
---
+++ src/main.rs
@@ -0,0 +1,24 @@
+pub struct VecNumber<'s> {
+    pub vec_number: Vec<Number<'s>>,
+    pub auxiliary_object: &'s Vec<usize>,
+}
+
+#[derive(Clone)]
+pub struct Number<'s> {
+    pub number: &'s usize,
+}
+
+impl<'s> VecNumber<'s> {
+    pub fn vec_number_iterable_per_item_in_auxiliary_object(
+        &self,
+    ) -> impl Iterator<Item = (&'s usize, impl Iterator<Item = &Number<'s>> + Clone)> {
+        self.auxiliary_object.iter().map(move |n| {
+            let iter_number = self.vec_number.iter();
+            (n, iter_number)
+        })
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
